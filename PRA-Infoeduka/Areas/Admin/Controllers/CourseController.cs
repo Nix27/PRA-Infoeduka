3f424b23d0230f3d33972866c18266222d4e8169
Nix27 <nikolazecic2468@gmail.com>
2023-05-31 16:00:35 +0200

    Dodan controller za course

---
+++ PRA-Infoeduka/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRA_Infoeduka.Areas.Admin.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult AllCourses()
+        {
+            return View();
+        }
+    }
+}
